720c8bbd9c2d2996e2174633ce5507077a03fd6b
yadnya2227 <99480610+yadnya2227@users.noreply.github.com>
2022-02-11 17:26:30 +0530

    Add files via upload

---
+++ yp_addtorem.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class yp_addtorem
+{
+   public static void main(String args[])
+   {
+     int a,b,add,sub,multi,div,rem;
+	 System.out.println("\n Enter Two Numbers");
+	 Scanner scan=new Scanner(System.in);
+	 a=scan.nextInt();
+	 b=scan.nextInt();
+	 add=a+b;
+	 sub=a-b;
+	 multi=a*b;
+	 div=a/b;
+	 rem=a%b;
+	 System.out.println("\n a= "+a);
+	 System.out.println("\n b= "+b);
+	 System.out.println("\n add= "+add);
+	 System.out.println("\n sub=  "+sub);
+	 System.out.println("\n multi= "+multi);
+	 System.out.println("\n div= "+div);
+	 System.out.println("\n rem= "+rem);
+	 }
+}
+	 

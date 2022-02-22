33775ca8d3e18cc7c51a3544623b928e3380a9f5
yadnya2227 <99480610+yadnya2227@users.noreply.github.com>
2022-02-22 09:37:21 +0530

    Update str_rev.java

--- str_rev.java
+++ str_rev.java
@@ -2,7 +2,7 @@ public class str_rev
 {
    public static void main(String args[])
    {
-      StringBuffer sb= new StringBuffer("yadnya");
+      StringBuffer sb= new StringBuffer("computer");
 	  sb.reverse();
 	  System.out.println(sb);
 	  }

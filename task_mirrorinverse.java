30545980bec23609a7ded9388a9f450f295453b6
yadnya2227 <99480610+yadnya2227@users.noreply.github.com>
2022-02-22 21:16:29 +0530

    Add files via upload

---
+++ task_mirrorinverse.java
@@ -0,0 +1,20 @@
+public class task_mirrorinverse
+{
+   static boolean isMirrorInverse(int arr[])
+   {
+   for(int i=0;i<arr.length;i++)
+   {
+       if(arr[arr[i]]!=i)
+	       return false;
+     }
+	 return true;
+  }
+public static void main(String args[])
+{
+   int arr[]={1,2,3,0};
+   if(isMirrorInverse(arr))
+       System.out.println("YES");
+   else
+       System.out.println("NO");
+ }
+}
